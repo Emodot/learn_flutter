8f4ce86b7f5a859bfeef7322a807d48e9412d438
David Emaye <emayeodavid@gmail.com>
2023-02-20 12:22:22 +0100

    added launcher icon package

--- lib/main.dart
+++ lib/main.dart
@@ -14,104 +14,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String btnName = 'Read More';
-  int currentIndex = 0;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        // drawer: NavDrawer(),
-        appBar: AppBar(
-          title: Text('Chekker'),
-        ),
-        body: Center(
-          child: currentIndex == 0
-              ? Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  color: Colors.orange,
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                foregroundColor: Colors.white,
-                                backgroundColor: Colors.green),
-                            onPressed: () {
-                              setState(() {
-                                btnName = 'Thanks';
-                              });
-                            },
-                            child: Text(btnName),
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                btnName = 'Thanks';
-                              });
-                            },
-                            child: Text(btnName),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                btnName = 'Thanks';
-                              });
-                            },
-                            child: Text(btnName),
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                btnName = 'Thanks';
-                              });
-                            },
-                            child: Text(btnName),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                )
-              : Image.asset(
-                  'images/photo-1495344517868-8ebaf0a2044a.jpeg'),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          // ignore: prefer_const_literals_to_create_immutables
-          items: [
-            BottomNavigationBarItem(
-              label: 'Home',
-              icon: Icon(Icons.home),
-            ),
-            BottomNavigationBarItem(
-              label: 'User',
-              icon: Icon(Icons.account_circle_rounded),
-            ),
-            BottomNavigationBarItem(
-              label: 'Settings',
-              icon: Icon(Icons.settings),
-            ),
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
-          },
-        ),
-      ),
+      home: HomePage()
     );
   }
 }
@@ -162,6 +71,111 @@ class NavDrawer extends StatelessWidget {
             onTap: () => {Navigator.of(context).pop()},
           ),
         ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+  
+  String btnName = 'Read More';
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // drawer: NavDrawer(),
+        appBar: AppBar(
+          title: Text('Chekker'),
+        ),
+        body: Center(
+          child: currentIndex == 0
+              ? Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  color: Colors.orange,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                foregroundColor: Colors.white,
+                                backgroundColor: Colors.green),
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (BuildContext context) {
+                                return const UserPage();
+                              }));
+                              // setState(() {
+                              //   btnName = 'Thanks';
+                              // });
+                            },
+                            child: Text(btnName),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                            },
+                            child: Text(btnName),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                            },
+                            child: Text(btnName),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                            },
+                            child: Text(btnName),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              : Image.asset('images/photo-1495344517868-8ebaf0a2044a.jpeg'),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          // ignore: prefer_const_literals_to_create_immutables
+          items: [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: 'User',
+              icon: Icon(Icons.account_circle_rounded),
+            ),
+            BottomNavigationBarItem(
+              label: 'Settings',
+              icon: Icon(Icons.settings),
+            ),
+          ],
+          currentIndex: currentIndex,
+          onTap: (int index) {
+          },
+        ),
+      );
+  }
+}
+
+class UserPage extends StatelessWidget {
+  const UserPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // automaticallyImplyLeading: false,
       ),
     );
   }
